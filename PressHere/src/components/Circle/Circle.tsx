099347b7d6ac71d6cb41c01943bba68c82491faf
mrmikesoto <mr.mike.soto@gmail.com>
2023-11-17 15:03:25 -0600

    tappable circle

--- PressHere/src/components/Circle/Circle.tsx
+++ PressHere/src/components/Circle/Circle.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {View} from 'react-native';
+import {Alert, Text, TouchableHighlight, View} from 'react-native';
 import styles from './styles';
 
 type CircleProps = PropsWithChildren<{
   color: string;
 }>;
 
+const _onPressButton = () => {
+  Alert.alert('You tapped the button!');
+};
+
 const Circle = ({color}: CircleProps): JSX.Element => {
-  return <View style={styles[color]} />;
+  return (
+    <TouchableHighlight onPress={_onPressButton} underlayColor="white">
+      <View style={styles[color as keyof typeof Circle]} />
+    </TouchableHighlight>
+  );
 };
 
 export default Circle;
